db6a8a21c20c8a609ae435fb1b514a442973fe5f
Somel <someonelolifoxi@outlook.com>
2025-12-01 13:12:50 -0300

    removing passwords, not big deal since is just a school project

--- server/index.js
+++ server/index.js
@@ -34,9 +34,9 @@ const upload = multer({
 // DB pool
 const pool = mysql.createPool({
   host: process.env.DB_HOST || '127.0.0.1',
-  port: process.env.DB_PORT ? Number(process.env.DB_PORT) : 5000,
+  port: process.env.DB_PORT ? Number(process.env.DB_PORT) : 5000, //im using port 5000, but u can change it
   user: process.env.DB_USER || 'root',
-  password: process.env.DB_PASS || 'pintolao123',
+  password: process.env.DB_PASS || '{whoever is reading this code, use your own password here, i removed mine for security reasons}',
   database: process.env.DB_NAME || 'cidade_perfeita',
   waitForConnections: true,
   connectionLimit: 10,
